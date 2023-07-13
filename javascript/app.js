b49cfb0b17caef0ef7f8e8f009ed9f34a4bf2830
Paulo Souza <pauloldsouza09@gmail.com>
2023-07-13 12:26:55 +0100

    fiz algumas mudanças mas preciso rever todo o javascript antes de fazer algo dessa complexidade

---
+++ javascript/app.js
@@ -0,0 +1,3 @@
+const users = require("./users");
+
+console.log(users);
---
+++ javascript/users.js
@@ -0,0 +1,13 @@
+const users = [];
+const user = {
+  name: "Paulo Ricardo",
+  phone: "9244000000",
+  email: "pauloldsouza09@gmail.com",
+  password: "11223344",
+  id: "01",
+  pageAdmin: true,
+};
+
+users.push(user);
+
+export default users;
