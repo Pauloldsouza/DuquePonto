5714d23e4b530aa1e5504e127af04193059bf802
Paulo Souza <pauloldsouza09@gmail.com>
2023-07-18 14:40:43 +0100

    adicionei hora no botão de bater o ponto

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,7 @@
 import { User } from "./users.js";
+const time = new Date();
+const hora = time.getHours();
+const min = time.getMinutes();
 const loggedUser = localStorage.getItem("loggedUser");
 const currentUser = JSON.parse(loggedUser);
 const userName = document.querySelector(".user-name");
@@ -10,7 +13,7 @@ startWorkingBtn.addEventListener("click", startWorking);
 function startWorking() {
   const buttonDiv = document.querySelector(".button");
   buttonDiv.innerHTML = `Good Job! 
-  You started working!`;
+  You started working at ${hora + "h" + min + "min"}!`;
 }
 
 document.querySelector(".exit").addEventListener("click", function () {
